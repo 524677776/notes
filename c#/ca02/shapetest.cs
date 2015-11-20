5d657c56aeef628097c0f8d5b50280d81b2369fe
zhangxiuyun <zhangxiuyun@shinezone.com>
2015-11-20 15:06:05 +0800

    del note

--- c#/ca02/shapetest.cs
+++
@@ -1,8 +0,0 @@
-public class TestClass
-{
-    static void Main()
-	{
-		Shape sa = new Square(5, "Square #1");
-		System.Console.WriteLine(sa);
-	}
-}
--- 数据结构算法与应用C++语言描述/3/Exception.cpp
+++
@@ -1,8 +0,0 @@
-#include "Exception.h"
-
-void my_new_handler()
-{
-	throw NoMem();
-}
-
-new_handler Old_Handler = std::set_new_handler(my_new_handler);
